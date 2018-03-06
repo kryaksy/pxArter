df4a5c20e53a5902bd91b8b5ba92e33d6329d92a
Ibrahim Koray Aksoy <ib.koray@gmail.com>
2018-03-06 15:55:46 +0300

    Added makeGrid() and Submit Listener

--- designs.js
+++ designs.js
@@ -1,10 +1,32 @@
 // Select color input
+var colorInput = document.getElementById('colorPicker').value;
+var inputH = document.getElementById('inputHeight');
+var inputW = document.getElementById('inputWeight');
+
 // Select size input
 
-// When size is submitted by the user, call makeGrid()
-
-function makeGrid() {
-
-// Your code goes here!
-
+//
+// Functions
+//
+function makeGrid(x,y) {
+  for (var i = 0; i < x; i++) {
+    var row = document.createElement("tr");
+    for (var j = 0; j < y; j++) {
+      var column = document.createElement("td");
+      row.appendChild(column);
+    }
+    document.querySelector('#pixelCanvas').appendChild(row);
+  }
 }
+
+//
+// Listeners
+//
+document.querySelector('#sizePicker').addEventListener('submit', function(e){
+  e.preventDefault();
+  const N = inputH.value;
+  const M = inputW.value;
+  makeGrid(N,M);
+});
+
+// TODO: Color Listener
