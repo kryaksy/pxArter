457b7bee5f5e10dce927e85262b80045b4bf80dd
kryaksy <ib.koray@gmail.com>
2018-03-07 13:47:15 +0300

    added submit listener to call makeGrid()

--- designs.js
+++ designs.js
@@ -6,6 +6,10 @@ var N = document.querySelector('#inputHeight').value;
 var M = document.querySelector('#inputWeight').value;
 
 // When size is submitted by the user, call makeGrid()
+document.querySelector('#sizePicker').addEventListener('submit', function(e) {
+	e.preventDefault();
+	makeGrid();
+});
 
 function makeGrid() {
 
