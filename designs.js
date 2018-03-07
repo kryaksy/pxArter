d6eba13be15db3c991d25f10fc1b4663b68269a4
kryaksy <ib.koray@gmail.com>
2018-03-07 15:10:00 +0300

    Added todo tasks to makeGrid and closed test logs

--- designs.js
+++ designs.js
@@ -14,26 +14,28 @@ const newTD = document.createElement('td');
 
 // When size is submitted by the user, call makeGrid()
 document.querySelector('#sizePicker').addEventListener('submit', function(e) {
+
 	e.preventDefault();
 	makeGrid();
 
-	// TEST
-	console.log('submit listener works!');
+	// TEST: console.log('submit listener works!');
 });
 
 function makeGrid() {
 
 	// Your code goes here!
 	for (var i = 0; i < N.value; i++) {
+		// TODO: Append TR to pxCanvas
 		for (var j = 0; j < M.value; j++) {
 
-			// TEST
-			console.log(i,j);
+			// TODO: Append TD to TR.lastChild
+
+			// TEST: console.log(i,j);
 
 		}
+
 	}
-	// TEST
-	console.log('makeGrid() works!');
+	// TEST: console.log('makeGrid() works!');
 
 }
 
