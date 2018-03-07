986a5bac14f0789ad22b18701089a88471dbc96d
kryaksy <ib.koray@gmail.com>
2018-03-07 14:03:56 +0300

    added for loops inside makeGrid function

--- designs.js
+++ designs.js
@@ -2,19 +2,27 @@
 var color = document.querySelector('#colorPicker').value;
 
 // Select size input
-var N = document.querySelector('#inputHeight').value;
-var M = document.querySelector('#inputWeight').value;
+var N = document.querySelector('#inputHeight');
+var M = document.querySelector('#inputWeight');
 
 // When size is submitted by the user, call makeGrid()
 document.querySelector('#sizePicker').addEventListener('submit', function(e) {
 	e.preventDefault();
 	makeGrid();
+
+	// TEST
 	console.log('submit listener works!');
 });
 
 function makeGrid() {
 
 	// Your code goes here!
+	for (var i = 0; i < N.value; i++) {
+		for (var j = 0; j < M.value; j++) {
+			
+		}
+	}
+	// TEST
 	console.log('makeGrid() works!');
 
 }
