d24425246fe5b7712eecd0df94bca4ca4753b158
kryaksy <ib.koray@gmail.com>
2018-03-08 15:49:46 +0300

    pxArter2 designs.js added

--- designs.js
+++ designs.js
@@ -1,43 +1,61 @@
 // Select color input
-var pxColor = document.querySelector('#colorPicker').value;
+const pxColor = document.querySelector('#colorPicker');
 
 // Select size input
-var N = document.querySelector('#inputHeight');
-var M = document.querySelector('#inputWidth');
+var canvasHeight = document.querySelector('#inputHeight');
+var canvasWidth = document.querySelector('#inputWidth');
 
 // Select Elements
 const pxCanvas = document.querySelector('#pixelCanvas');
+const body = document.querySelector('body');
 
 // When size is submitted by the user, call makeGrid()
 document.querySelector('#sizePicker').addEventListener('submit', function(e) {
-
 	e.preventDefault();
 	makeGrid();
-
 });
 
-// When cell is clicked by the user, call makePx()
-document.querySelector('#pixelCanvas').addEventListener('click', makePx);
+// Continuous Drawing
 
-// FUNCTIONS
+let mouseIsDown = false;
+
+document.querySelector('#pixelCanvas').addEventListener('mousemove', function (e) {
+  if (mouseIsDown) {
+		makePx(e);
+	}
+});
+
+document.querySelector('#pixelCanvas').addEventListener('mousedown', function () {
+	mouseIsDown = true;
+});
+
+document.querySelector('#pixelCanvas').addEventListener('mouseup', function () {
+  mouseIsDown = false;
+});
+
+// Make Grid
 function makeGrid() {
-
+	const N = canvasHeight.value;
+	const M = canvasWidth.value;
+	var cellSize = Math.floor(window.innerWidth / M);
 	pxCanvas.innerHTML = "";
-	for (var i = 0; i < N.value; i++) {
 
+	for (var i = 0; i < N; i++) {
 		let newTR = document.createElement('tr');
 		pxCanvas.appendChild(newTR);
-		for (var j = 0; j < M.value; j++) {
 
+		for (var j = 0; j < M; j++) {
 			let newTD = document.createElement('td');
+			window.td = newTD;
+			newTD.style.height = cellSize + "px";
+			newTD.style.width = cellSize + "px";
 			pxCanvas.getElementsByTagName('tr')[i].appendChild(newTD);
-
 		}
-
 	}
-
+	body.style.backgroundColor = pxColor.value/10;
 }
 
+// Make Pixel
 function makePx(e) {
-	e.target.style.backgroundColor = pxColor;
+	e.target.style.backgroundColor = pxColor.value;
 }
