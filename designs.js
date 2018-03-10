1c3ec1b944ad96779ad02e87ef9fabcff7483f27
Ibrahim Koray Aksoy <ib.koray@gmail.com>
2018-03-10 13:21:22 +0300

    used atom beautify - no functional change

--- designs.js
+++ designs.js
@@ -13,63 +13,63 @@ const h1 = document.querySelector('h1');
 
 // When size is submitted by the user, call makeGrid()
 document.querySelector('#sizePicker').addEventListener('submit', function(e) {
-	e.preventDefault();
-	makeGrid();
-	body.style.paddingTop = '0';
-	drawPx();
+  e.preventDefault();
+  makeGrid();
+  body.style.paddingTop = '0';
+  drawPx();
 });
 
 // Draw Pixel, Continuous Drawing
 function drawPx() {
-	let mouseIsDown = false;
+  let mouseIsDown = false;
 
-	pxCanvas.addEventListener('mousemove', function(e) {
-		mouseIsDown && makePx(e);
-	});
+  pxCanvas.addEventListener('mousemove', function(e) {
+    mouseIsDown && makePx(e);
+  });
 
-	pxCanvas.addEventListener('click', function(e) {
-		makePx(e);
-	});
+  pxCanvas.addEventListener('click', function(e) {
+    makePx(e);
+  });
 
-	pxCanvas.addEventListener('mousedown', function() {
-		mouseIsDown = true;
-	});
+  pxCanvas.addEventListener('mousedown', function() {
+    mouseIsDown = true;
+  });
 
-	pxCanvas.addEventListener('mouseup', function() {
-		mouseIsDown = false;
-	});
+  pxCanvas.addEventListener('mouseup', function() {
+    mouseIsDown = false;
+  });
 }
 
 // Make Grid
 function makeGrid() {
-	const makeGridStart = performance.now();
+  const makeGridStart = performance.now();
 
-	const N = canvasHeight.value;
-	const M = canvasWidth.value;
-	const tBody = document.createDocumentFragment();
-	var cellSize = Math.floor(window.innerWidth / M);
+  const N = canvasHeight.value;
+  const M = canvasWidth.value;
+  const tBody = document.createDocumentFragment();
+  var cellSize = Math.floor(window.innerWidth / M);
 
-	pxCanvas.innerHTML = "";
-	for (var i = 0; i < N; i++) {
-		let newTR = document.createElement('tr');
-		let load = Math.floor(i / (N - 1) * 100);
-		for (var j = 0; j < M; j++) {
-			let newTD = document.createElement('td');
-			window.td = newTD;
-			newTD.style.height = cellSize + "px";
-			newTD.style.width = cellSize + "px";
-			newTR.appendChild(newTD);
-		}
-		tBody.appendChild(newTR);
-	}
-	pxCanvas.appendChild(tBody);
-	const makeGridEnd = performance.now();
-	var makeGridInterval = Math.floor(makeGridEnd - makeGridStart);
-	console.log(makeGridInterval + "ms");
+  pxCanvas.innerHTML = "";
+  for (var i = 0; i < N; i++) {
+    let newTR = document.createElement('tr');
+    let load = Math.floor(i / (N - 1) * 100);
+    for (var j = 0; j < M; j++) {
+      let newTD = document.createElement('td');
+      window.td = newTD;
+      newTD.style.height = cellSize + "px";
+      newTD.style.width = cellSize + "px";
+      newTR.appendChild(newTD);
+    }
+    tBody.appendChild(newTR);
+  }
+  pxCanvas.appendChild(tBody);
+  const makeGridEnd = performance.now();
+  var makeGridInterval = Math.floor(makeGridEnd - makeGridStart);
+  console.log(makeGridInterval + "ms");
 }
 
 // Make Pixel
 function makePx(e) {
-	e.target.style.backgroundColor = pxColor.value;
-	e.target.style.border = "1px dotted" + pxColor.value;
+  e.target.style.backgroundColor = pxColor.value;
+  e.target.style.border = "1px dotted" + pxColor.value;
 }
